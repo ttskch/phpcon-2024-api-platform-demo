71ff45a224a329598f92cb83ab0e302061ad3f22
Takashi Kanemoto <ttskch@gmail.com>
2024-12-21 14:26:09 +0900

    feat: 🎸 fix OpenAPI property type

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Put;
@@ -12,6 +13,7 @@ use Illuminate\Database\Eloquent\Model;
 #[Get(uriTemplate: '/articles/random', provider: ArticleRandomProvider::class)]
 #[Put(uriTemplate: '/articles/{id}/publication', deserialize: false, processor: ArticlePublishProcessor::class)]
 #[ApiResource]
+#[ApiProperty(schema: ['type' => 'boolean'], property: 'published')]
 class Article extends Model
 {
     //
